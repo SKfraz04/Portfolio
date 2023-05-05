799a881eafcb9a3fd16ad18b0b1797bf087788f5
SKFraz <116868571+SKfraz04@users.noreply.github.com>
2023-05-05 16:27:37 +0530

    linked profile URL

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon4 from "../assets/img/github-mark-white.svg";
 
 export const Footer = () => {
   return (
@@ -12,13 +12,14 @@ export const Footer = () => {
         <Row className="align-items-center">
           <MailchimpForm />
           <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
+          <h2>SAQUIB FRAZ</h2>
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+            <a href="https://www.facebook.com/sk.fraz"><img src={navIcon1} alt="link" /></a>
+                <a href="https://www.facebook.com/sk.fraz"><img src={navIcon2} alt="fb" /></a>
+                <a href="https://www.instagram.com/sk.fraz/"><img src={navIcon3} alt="insta" /></a>
+                <a href="https://github.com/SKfraz04"><img src={navIcon4} alt="insta" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
